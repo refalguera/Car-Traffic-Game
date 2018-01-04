ddde0785aed813d71ac34cb9ae87be724fdfae7e
Renata Falguera Gonçalves <renata.falguera@gmail.com>
2018-01-04 18:31:42 -0200

    Add files via upload

---
+++ WalkPerson.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkPerson : MonoBehaviour {
+
+    public float speed = 5;
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        
+        anim.
+        transform.position += new Vector3(speed, 0, 0);
+   
+	}
+}
---
+++ spawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour {
+    public GameObject[] enemies;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+    int randEnemy;
+    void Start () {
+        StartCoroutine(waitSpawner());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+	}
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (!stop)
+        {
+            randEnemy = Random.Range(0, 2);
+            Vector3 spawnPosition = new Vector3(spawnValues.x, spawnValues.y, spawnValues.z);
+            Instantiate(enemies[randEnemy], spawnPosition , gameObject.transform.rotation);
+            yield return new WaitForSeconds(spawnWait);
+                 
+        }
+    }
+}
